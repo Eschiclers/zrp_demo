1ef68acb1ae8e249596bec7130215671a053ddaf
Chicle <chicle@protonmail.com>
2022-03-16 20:16:01 +0100

    add: fxmanifest básico

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Chicle <hola@chicle.dev> (https://chicle.dev)'
+description 'ZRP Framework Demo'
+version '1.0.0'
+
+server_scripts {
+  'server/main.lua',
+  'server/commands.lua',
+}
+
+client_scripts {
+  'client/main.lua',
+}
